68c326e5b5fa6d2bdada2b741fa732dd663ed9ea
hoseyn <87471826+hoseyn-chegeni@users.noreply.github.com>
2024-07-06 20:54:45 +0330

    setup redis and celery for craete queue in job_service

--- jobs/main.py
+++ jobs/main.py
@@ -2,9 +2,26 @@ from fastapi import FastAPI
 from sql_app import models
 from sql_app.database import engine
 from sql_app import main
+from celery import Celery
 
 models.Base.metadata.create_all(bind=engine)
+
+celery = Celery(
+    __name__,
+    broker='redis://redis:6379/0',
+    backend='redis://redis:6379/0'
+)
 
 app = FastAPI()
 
 app.include_router(main.router, prefix="/api/v1")
+
+
+@app.post("/task")
+def run_task():
+    task = example_task.delay()
+    return {"task_id": task.id}
+
+@celery.task
+def example_task():
+    return {"status": "Task completed"}
