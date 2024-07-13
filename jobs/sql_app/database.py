19b50f23093894dbb02a9b087ff8b73fee0b32cb
hoseyn <87471826+hoseyn-chegeni@users.noreply.github.com>
2024-07-13 15:48:22 +0330

    connect celery and job_service to postgres db

--- jobs/sql_app/database.py
+++ jobs/sql_app/database.py
@@ -2,16 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = (
-    "sqlite:///./test.db"  # For SQLite. Change this to your actual database URL.
-)
+SQLALCHEMY_DATABASE_URL = "postgresql://fastapi_user:fastapi_password@db/job_service_db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
