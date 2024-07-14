1ef7e419cc653d0d36b8a63a793f5745d5c4dbbb
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-07-14 18:59:51 +0330

    add send job method

--- jobs/main.py
+++ jobs/main.py
@@ -29,9 +29,7 @@ async def send_job(job_request: JobCreate = Body(...), db: Session = Depends(get
         raise HTTPException(
             status_code=400, detail=f"City name {e.args[0]} is not recognized"
         )
-
     db_job = crud.create_job(db, job_request, city_ids)
-
     job_json = json.dumps(
         {
             "id": db_job.id,
@@ -42,9 +40,7 @@ async def send_job(job_request: JobCreate = Body(...), db: Session = Depends(get
             "status": db_job.status,
         }
     )
-
     redis_client.lpush("jobs_queue", job_json)
-
     return {"message": "Job sent to the queue", "data": job_json}
 
 
@@ -54,5 +50,16 @@ async def get_queue_instances():
     instances = [
         json.loads(redis_client.lindex("jobs_queue", i)) for i in range(queue_length)
     ]
-
     return {"instances_in_queue": instances}
+
+
+@app.get("/send_job")
+async def send_job():
+    try:
+        job_json = redis_client.rpop("jobs_queue")
+        if not job_json:
+            raise HTTPException(status_code=404, detail="No jobs in the queue")
+        job = json.loads(job_json)
+        return job
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
